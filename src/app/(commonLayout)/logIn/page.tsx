1deff932035e21ef3bf229c435209ff22367a5e4
mdarafatyeasin <yeasinarafat54239@gmail.com>
2026-02-01 00:51:35 +0600

    comple the basic auth setup

--- src/app/(commonLayout)/logIn/page.tsx
+++ src/app/(commonLayout)/logIn/page.tsx
@@ -1,8 +1,117 @@
+"use client"
 
-export default function Login() {
+import { Button } from "@/components/ui/button"
+import {
+    Card,
+    CardAction,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { signIn } from "@/lib/auth-client"
+
+export default function LogIn() {
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const formData = new FormData(e.currentTarget);
+        const email = formData.get("email") as string;
+        const password = formData.get("password") as string;
+
+
+
+        try {
+            const res = await signIn.email({
+                email,
+                password,
+                callbackURL:"http://localhost:3000"
+            })
+            
+            console.log(res);
+        } catch (err) {
+            console.log(err);
+        }
+        // const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/auth/sign-in/email`, {
+        //     method: "POST",
+        //     headers: {
+        //         "Content-Type": "application/json",
+        //     },
+        //     credentials: "include",
+        //     body: JSON.stringify({
+        //         email, password
+        //     })
+        // })
+
+        // const data = await res.json();
+        // console.log("Response:", data);
+
+        // if (!res.ok) {
+        //     throw new Error(data.message || "Login failed");
+        // }
+
+        // console.log("✅ Login success", data);
+    };
+
+
+
     return (
-        <div>
-            this is log in page!!
+        <div className="my-20">
+            <Card className="w-full max-w-sm mx-auto">
+                <CardHeader>
+                    <CardTitle>Login to your account</CardTitle>
+                    <CardDescription>
+                        Enter your email below to login to your account
+                    </CardDescription>
+                    <CardAction>
+                        <Button variant="link">Sign Up</Button>
+                    </CardAction>
+                </CardHeader>
+                <CardContent>
+                    <form onSubmit={handleSubmit}>
+                        <div className="flex flex-col gap-6">
+                            <div className="grid gap-2">
+                                <Label htmlFor="email">Email</Label>
+                                <Input
+                                    id="email"
+                                    name="email"
+                                    type="email"
+                                    placeholder="m@example.com"
+                                    required
+                                />
+                            </div>
+                            <div className="grid gap-2">
+                                <div className="flex items-center">
+                                    <Label htmlFor="password">Password</Label>
+                                    <a
+                                        href="#"
+                                        className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                                    >
+                                        Forgot your password?
+                                    </a>
+                                </div>
+                                <Input
+                                    id="password"
+                                    name="password"
+                                    type="password"
+                                    required
+                                />
+                            </div>
+                        </div>
+                        <Button type="submit" className="w-full mt-6">
+                            Login
+                        </Button>
+                    </form>
+                </CardContent>
+                <CardFooter className="flex-col gap-2">
+
+                    <Button variant="outline" className="w-full">
+                        Login with Google
+                    </Button>
+                </CardFooter>
+            </Card>
         </div>
-    );
+    )
 }
